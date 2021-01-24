ce6b1087b481a966d919392fc7c4e279f9ba85da
Mazhar910 <mazharuddin910@gmail.com>
2021-01-24 16:44:53 +0500

    comments addes

--- AI_Attendance.py
+++ AI_Attendance.py
@@ -16,7 +16,7 @@ for cl in myList:
     classNames.append(os.path.splitext(cl)[0])
 print(classNames)
  
-def findEncodings(images):
+def findEncodings(images): #function to encode the images 
     encodeList = []
     for img in images:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -24,7 +24,7 @@ def findEncodings(images):
         encodeList.append(encode)
     return encodeList
  
-def markAttendance(name):
+def markAttendance(name): #function to write roll no.s of present students
     with open('thefile.csv','r+') as f:
         myDataList = f.readlines()
         nameList = []
@@ -36,20 +36,13 @@ def markAttendance(name):
             dtString = now.strftime('%H:%M:%S')
             f.writelines(f'\n{name},{dtString}')
  
-#### FOR CAPTURING SCREEN RATHER THAN WEBCAM
-# def captureScreen(bbox=(300,300,690+300,530+300)):
-#     capScr = np.array(ImageGrab.grab(bbox))
-#     capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
-#     return capScr
- 
 encodeListKnown = findEncodings(images)
 print('Encoding Complete')
  
 cap = cv2.VideoCapture(0)
  
-while True:
+while True: #loop to compare the webcam recorded face with dataset
     success, img = cap.read()
-    #img = captureScreen()
     imgS = cv2.resize(img,(0,0),None,0.25,0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
  
@@ -57,20 +50,17 @@ while True:
     encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
  
     for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
-        #print(faceDis)
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFace) #compare the encoded image to the webcam recorded image
+        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace) #calculate the face distance
         matchIndex = np.argmin(faceDis)
- 
         if matches[matchIndex]:
             name = classNames[matchIndex].upper()
-            #print(name)
             y1,x2,y2,x1 = faceLoc
             y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
             cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
             cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
-            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
-            markAttendance(name)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2) #putting a rectangular frame around face
+            markAttendance(name) #sendind the recognized roll no. to function which writes attendance
  
     cv2.imshow('Webcam',img)
     cv2.waitKey(1)
