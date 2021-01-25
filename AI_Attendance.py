e08f1a08dd9a27d3d7f9b97a847a8250f6d73805
Mazhar910 <mazharuddin910@gmail.com>
2021-01-25 14:18:24 +0500

    csv file creator added

--- AI_Attendance.py
+++ AI_Attendance.py
@@ -3,12 +3,16 @@ import numpy as np
 import face_recognition
 import os
 from datetime import datetime
- 
+import csv
+
+csvFileName=input("Enter the name of csv file : ")
 path = 'ImagesAttendance'
 imagesData = []
 classNames = []
 myList = os.listdir(path)
 print(myList)
+with open(csvFileName+'.csv','w',newline='') as file:
+    writer = csv.writer(file)
 for cl in myList:
     curImg = cv2.imread(f'{path}/{cl}')
     imagesData.append(curImg)
@@ -24,7 +28,8 @@ def findEncodings(imagesData): #function to encode the images
     return encodeList
  
 def markAttendance(name): #function to write roll no.s of present students
-    with open('thefile.csv','r+') as f:
+
+    with open(csvFileName+'.csv','r+') as f:
         myDataList = f.readlines()
         nameList = []
         for line in myDataList:
@@ -34,15 +39,15 @@ def markAttendance(name): #function to write roll no.s of present students
             now = datetime.now()
             dtString = now.strftime('%H:%M:%S')
             f.writelines(f'\n{name},{dtString}')
- 
+            
 encodeListKnown = findEncodings(imagesData)
-print('Encoding Complete')
+print("Encoding complete")
  
 cap = cv2.VideoCapture(0)
  
 while True: #loop to compare the webcam recorded face with dataset
     success, img = cap.read()
-    imgS = cv2.resize(img,(0,0),None,0.26,0.26)
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
  
     facesCurFrame = face_recognition.face_locations(imgS)
