be500a8782fac824bdb6edb0233324048ecf2e35
Mazhar910 <mazharuddin910@gmail.com>
2021-01-27 19:22:57 +0500

    changes done

--- AI_Attendance.py
+++ AI_Attendance.py
@@ -5,29 +5,32 @@ import os
 from datetime import datetime
 import csv
 
+MODEL = 'hog'
+TOLERANCE = 0.8
 csvFileName=input("Enter the name of csv file : ")
 row_list = [["ROLL NUMBER", "TIME"]]
-path = 'ImagesAttendance'
+path = 'DATASETOFIMAGES'
 imagesData = []
 classNames = []
 myList = os.listdir(path)
-print(myList)
+#print(myList)
 with open(csvFileName+'.csv','w',newline='') as file:
     writer = csv.writer(file)
     writer.writerows(row_list)
-for cl in myList:
-    curImg = cv2.imread(f'{path}/{cl}')
+    print("CSV File Created")
+for element in myList:
+    curImg = cv2.imread(f'{path}/{element}')
     imagesData.append(curImg)
-    classNames.append(os.path.splitext(cl)[0])
-print(classNames)
+    classNames.append(os.path.splitext(element)[0])
+#print(classNames)
  
 def findEncodings(imagesData): #function to encode the images 
-    encodeList = []
+    encodedList = []
     for img in imagesData:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         encode = face_recognition.face_encodings(img)[0]
-        encodeList.append(encode)
-    return encodeList
+        encodedList.append(encode)
+    return encodedList
  
 def markAttendance(name): #function to write roll no.s of present students
 
@@ -43,26 +46,27 @@ def markAttendance(name): #function to write roll no.s of present students
             f.writelines(f'\n{name},{dtString}')
             
 encodeListKnown = findEncodings(imagesData)
-print("Encoding complete")
+print("Encoding Complete")
  
 cap = cv2.VideoCapture(0)
+print("Opening Camera")
  
 while True: #loop to compare the webcam recorded face with dataset
     success, img = cap.read()
-    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.resize(img,(0,0),None,0.17,0.17)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
  
-    facesCurFrame = face_recognition.face_locations(imgS)
+    facesCurFrame = face_recognition.face_locations(imgS,model=MODEL)
     encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
  
     for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown,encodeFace) #compare the encoded image to the webcam recorded image
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFace,TOLERANCE) #compare the encoded image to the webcam recorded image
         faceDis = face_recognition.face_distance(encodeListKnown,encodeFace) #calculate the face distance
         matchIndex = np.argmin(faceDis)
         if matches[matchIndex]:
             name = classNames[matchIndex].upper()
             y1,x2,y2,x1 = faceLoc
-            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+            y1, x2, y2, x1 = y1*6,x2*6,y2*6,x1*6
             cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
             cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
             cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2) #putting a rectangular frame around face
